ec9809236b733d924d0d5926c450a0db9b798740
takaaki <soci0926@gmail.com>
2024-07-07 14:29:46 +0900

    output hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import ImageGallery from "@/components/ImageGallery";
 
-import TodoList from "@/components/TodoList";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
