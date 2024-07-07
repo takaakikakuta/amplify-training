065df59696bbbce786ae2a2431a370bf9bcd0402
takaaki <soci0926@gmail.com>
2024-07-07 14:45:58 +0900

    output

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -2,10 +2,10 @@
 
 import React, { useEffect, useState } from 'react'
 
-// import { Amplify } from 'aws-amplify';
-// import outputs from '../../amplify_outputs.json';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
 
-// Amplify.configure(outputs);
+Amplify.configure(outputs);
 
 import type { Schema } from '../../amplify/data/resource'
 import { generateClient } from 'aws-amplify/data'
